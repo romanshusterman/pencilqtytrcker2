e56bc7354e7a320dcb34345da16a1036199226e1
romanshusterman <romanshusterman@gmail.com>
2023-01-07 18:23:28 +0200

    Create python_script_2.py

---
+++ python_script_2.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[154]:
+
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import os
+import datetime
+
+
+# In[175]:
+
+
+PATH_GD = '⁨usr/⁨bin/chromedriver⁩'
+
+
+# In[176]:
+
+
+driver = webdriver.Chrome(PATH_GD)
+
+
+# In[177]:
+
+
+website = driver.get('https://www.ilmakiage.co.il/mineral-lip-pencil-4043')
+
+
+# In[178]:
+
+
+for _ in range(100):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+soup1 = driver.page_source.encode("utf-8")
+
+driver.close()
+
+
+# In[179]:
+
+
+soup = BeautifulSoup(soup1, 'lxml')
+
+
+# In[180]:
+
+
+try:
+    soup.find('span', class_='qtyValidate_color', style='display: block;').contents[0] == 'הכמות המבוקשת אינה קיימת'
+    qty_validate = soup.find('span', class_='qtyValidate_color', style='display: block;').contents[0]
+    result = f'Ooops, the stock is empty ({qty_validate}). Open the app later to check quantity \n'
+except AttributeError as err:
+    result = "Tut sheli, it'shopping time, your favorite lip pencil is in stock \n https://www.ilmakiage.co.il/mineral-lip-pencil-4043 \n"
+
+
+# In[190]:
+
+current_time = datetime.datetime.now()
+with open('rand2.txt', mode='a') as file:
+    file.write(f"{current_time}, {result}")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
